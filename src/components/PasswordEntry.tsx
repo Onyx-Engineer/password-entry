211f734e104fc879fadd933add6d6c837aa028fb
Onyx-Engineer <82298238+Onyx-Engineer@users.noreply.github.com>
2025-03-23 15:11:07 -0400

    [AWEB-1] Update base design

--- src/components/PasswordEntry.tsx
+++ src/components/PasswordEntry.tsx
@@ -4,23 +4,47 @@ import { debounce } from "lodash";
 const PasswordEntry: React.FC = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    console.log("name:", name);
     console.log("password: ", password);
     console.log("confirm password: ", confirmPassword);
   };
 
   return (
     <>
-      <section className={"m-4"}>
+      <section className={"flex flex-col m-4 p-4 round-md items-center my-10"}>
         <form
-          className={"flex flex-col card bg-gray-100 p-4"}
+          className={"flex flex-col card bg-gray-100 p-6 w-1/3"}
           onSubmit={handleSubmit}
         >
-          <div className={"flex flex-col py-4"}>
-            <label htmlFor="password">Password:</label>
+          <h1 className={"text-center text-3xl"}>Login</h1>
+          <div className={"flex flex-col pb-2"}>
+            <label className={"py-2"} htmlFor="name">
+              Name:
+            </label>
             <input
+              className={"bg-white rounded-sm p-2"}
+              onChange={debounce(
+                (e: React.FormEvent) =>
+                  setName((e.target as HTMLInputElement).value),
+                300
+              )}
+              tabIndex={0}
+              type="name"
+              id="name"
+              placeholder="Enter name"
+              required
+            />
+          </div>
+          <div className={"flex flex-col pb-2"}>
+            <label className={"py-2"} htmlFor="password">
+              Password:
+            </label>
+            <input
+              className={"bg-white rounded-sm p-2"}
               onChange={debounce(
                 (e: React.FormEvent) =>
                   setPassword((e.target as HTMLInputElement).value),
@@ -34,9 +58,12 @@ const PasswordEntry: React.FC = () => {
             />
           </div>
 
-          <div className={"flex flex-col py-4"}>
-            <label htmlFor="confirm-password">Confirm Password:</label>
+          <div className={"flex flex-col pb-2"}>
+            <label className={"py-2"} htmlFor="confirm-password">
+              Confirm Password:
+            </label>
             <input
+              className={"bg-white rounded-sm p-2"}
               onChange={debounce(
                 (e: React.FormEvent) =>
                   setConfirmPassword((e.target as HTMLInputElement).value),
@@ -49,7 +76,11 @@ const PasswordEntry: React.FC = () => {
               required
             />
           </div>
-          <button className={"bg-blue-500 py-1"} tabIndex={0} type="submit">
+          <button
+            className={"bg-blue-500 p-2 my-3 rounded-sm"}
+            tabIndex={0}
+            type="submit"
+          >
             Submit
           </button>
         </form>
