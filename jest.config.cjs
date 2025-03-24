e49a828772ac9f510dadaeef7da39be39512478a
Onyx-Engineer <82298238+Onyx-Engineer@users.noreply.github.com>
2025-03-23 21:30:45 -0400

    [AWEB-1] Fix test configuration

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
+  testEnvironment: "jsdom",
   testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
   transform: {
-    "^.+\\.[jt]sx?$": "ts-jest", // or 'ts-jest' for TypeScript projects
+    "^.+\\.[jt]sx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.app.json" }],
   },
   clearMocks: true,
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
