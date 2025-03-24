791e7b47eb7804e3477779272a80b8c53ba420f7
Onyx-Engineer <82298238+Onyx-Engineer@users.noreply.github.com>
2025-03-23 21:32:26 -0400

    [AWEB-1] Add error handling form validation

--- src/components/PasswordEntry.tsx
+++ src/components/PasswordEntry.tsx
@@ -1,16 +1,58 @@
 import React, { useState } from "react";
 import { debounce } from "lodash";
 
-const PasswordEntry: React.FC = () => {
+export type ValidationFieldsProps = {
+  name: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const validateFields = (submittedFields: ValidationFieldsProps) => {
+  const errors: ValidationFieldsProps = {
+    name: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  if (submittedFields.name.length < 2) {
+    errors.name = "Name must be at least 2 characters long";
+  }
+
+  if (submittedFields.password.length < 6) {
+    errors.password = "Password must be at least 6 characters long.";
+  }
+
+  if (submittedFields.confirmPassword !== submittedFields.confirmPassword) {
+    errors.confirmPassword = "Passwords do not match.";
+  }
+
+  return errors;
+};
+
+export const PasswordEntry: React.FC = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [name, setName] = useState("");
+  const [errors, setErrors] = useState<ValidationFieldsProps>({
+    name: "",
+    password: "",
+    confirmPassword: "",
+  });
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     console.log("name:", name);
     console.log("password: ", password);
     console.log("confirm password: ", confirmPassword);
+    const validateErrors = validateFields({ name, password, confirmPassword });
+    setErrors(validateErrors);
+    if (Object.keys(validateErrors).length === 0) {
+      console.log("Submitted");
+    }
+  };
+
+  const handleBlur = () => {
+    setErrors(validateFields({ name, password, confirmPassword }));
   };
 
   return (
@@ -32,12 +74,18 @@ const PasswordEntry: React.FC = () => {
                   setName((e.target as HTMLInputElement).value),
                 300
               )}
+              onBlur={handleBlur}
               tabIndex={0}
               type="name"
               id="name"
               placeholder="Enter name"
               required
             />
+            {errors.name && (
+              <p className="text-red-500" id="nameError">
+                {errors.name}
+              </p>
+            )}
           </div>
           <div className={"flex flex-col pb-2"}>
             <label className={"py-2"} htmlFor="password">
@@ -50,12 +98,18 @@ const PasswordEntry: React.FC = () => {
                   setPassword((e.target as HTMLInputElement).value),
                 300
               )}
+              onBlur={handleBlur}
               tabIndex={0}
               type="password"
               id="password"
               placeholder="Enter password"
               required
             />
+            {errors.password && (
+              <p className="text-red-500" id="passwordError">
+                {errors.password}
+              </p>
+            )}
           </div>
 
           <div className={"flex flex-col pb-2"}>
@@ -74,7 +128,13 @@ const PasswordEntry: React.FC = () => {
               id="confirm-password"
               placeholder="Confirm password"
               required
+              onBlur={handleBlur}
             />
+            {errors.confirmPassword && (
+              <p className="text-red-500" id="confirmPasswordErrors">
+                {errors.confirmPassword}
+              </p>
+            )}
           </div>
           <button
             className={"bg-blue-500 p-2 my-3 rounded-sm"}
